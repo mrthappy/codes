29f1381b859c80d12966c904106ccde54dc60644
mrthappy <mrthappymailz@gmail.com>
2018-12-26 16:37:46 +0100

    just added a new javascript

--- javascript/app.js
+++ javascript/app.js
@@ -1,36 +1,22 @@
-var app = angular.module("myApp", [ "ngMessages"]);
-// the controller allows one to add services from angular to be used on the viewed
-// directives allows you to change what happens on the webpage
+$(document).ready(function(){
+  // global object
+  let nav_anchors =$(".snchor a ");
+  let burger =$("#burger");
+  let main_nav=$(".main_nav");
+  burger.click(function(event){
+    return setTimeout(function(){
+      return new Promise(function((resolve,reject){
+      let counter ;
+      let num =Math.floor(Math.random()*100));
+      if(num >0){
+        console.log("this ist he proeoe")
+      }
 
-app.controller("controller", ["$scope", "$http", function($scope, $http) {
 
-
-}]);
-
-
-app.directive("formArea", function() {
-    return {
-        templateUrl: "form.php",
-        controller: "controller"
-    }
-});
-app.config(["$routeProvider", function($routeProvider) {
-
-    $routeProvider.
-    when("/", {
-        templateUrl: "form.php",
-        controller: "controller"
-
-    }).when("/register", {
-        templateUrl: "register.php",
-        controller: "controller"
-
-    }).
-    when("/password", {
-        templateUrl: "password.php"
-    }).
-
-    otherwise({
-        redirectoTo: "form.php"
+      }));
+    },3000).then(function(data){
+      console.log("this is the beser ");
     });
-}]);
+      event.preventDefaul();
+  });
+})
